0b9002deee5880379d57201f37dcfba1c2eaf366
Danna Cortes <66390576+Dannacortes@users.noreply.github.com>
2022-09-14 19:53:26 -0500

    Add files via upload
    Se agregaron las clases terminadas y se intento emplearlas pero no fue posible

---
+++ Departamento.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    public class Departamento
+    {
+        string departamentoElegido, departamentoBeneficiado;
+        string amazonas = "AMA";
+        string antioquia = "ANT";
+        string arauca = "ARA";
+        string atlantico = "ATL";
+        string bogota = "BOG";
+        string bolivar = "BOL";
+        string boyaca = "BOY";
+        string caldas = "CAL";
+        string caqueta = "CAQ";
+        string casanare = "CAS";
+        string cauca = "CAU";
+        string cesar = "CES";
+        string choco = "CHO";
+        string cordoba = "COR";
+        string cundinamarca = "CUN";
+        string guainia = "GUA";
+        string guaviare = "GUE";
+        string huila = "HUI";
+        string laguajira = "LAG";
+        string magdalena = "MAG";
+        string meta = "MET";
+        string nariño = "NAR";
+        string nortedesantander = "NSA";
+        string putumayo = "PUT";
+        string quindio = "QUI";
+        string risaralda = "RIS";
+        string SanAndresYProvidencia = "SAP";
+        string santander = "SAN";
+        string sucre = "SUC";
+        string tolima = "TOL";
+        string valledelcauca = "VCA";
+        string vaupes = "VAU";
+        string vichada = "VIC";
+
+ 
+
+        public string DepartamentoElegido { get => departamentoElegido; }
+
+        public void asignarCodeDep()
+        {
+            if (departamentoBeneficiado ==("amazonas"))
+            {
+                departamentoElegido = amazonas;
+            }
+            else if (departamentoBeneficiado ==("antioquia"))
+            {
+                departamentoElegido = antioquia;
+            }
+            else if (departamentoBeneficiado ==("arauca"))
+            {
+                departamentoElegido = arauca;
+            }
+            else if (departamentoBeneficiado ==("atlantico"))
+            {
+                departamentoElegido = atlantico;
+            }
+            else if (departamentoBeneficiado ==("bogota"))
+            {
+                departamentoElegido = bogota;
+            }
+            else if (departamentoBeneficiado ==("bolivar"))
+            {
+                departamentoElegido = bolivar;
+            }
+            else if (departamentoBeneficiado ==("boyaca"))
+            {
+                departamentoElegido = boyaca;
+            }
+            else if (departamentoBeneficiado ==("caldas"))
+            {
+                departamentoElegido = caldas;
+            }
+            else if (departamentoBeneficiado ==("caqueta"))
+            {
+                departamentoElegido = caqueta;
+            }
+            else if (departamentoBeneficiado ==("casanare"))
+            {
+                departamentoElegido = casanare;
+            }
+            else if (departamentoBeneficiado ==("cauca"))
+            {
+                departamentoElegido = cauca;
+            }
+            else if (departamentoBeneficiado ==("cesar"))
+            {
+                departamentoElegido = cesar;
+            }
+            else if (departamentoBeneficiado ==("choco"))
+            {
+                departamentoElegido = choco;
+            }
+            else if (departamentoBeneficiado ==("cordoba"))
+            {
+                departamentoElegido = cordoba;
+            }
+            else if (departamentoBeneficiado ==("cundinamarca"))
+            {
+                departamentoElegido = cundinamarca;
+            }
+            else if (departamentoBeneficiado ==("guainia"))
+            {
+                departamentoElegido = guainia;
+            }
+            else if (departamentoBeneficiado ==("guaviare"))
+            {
+                departamentoElegido = guaviare;
+            }
+            else if (departamentoBeneficiado ==("huila"))
+            {
+                departamentoElegido = huila;
+            }
+            else if (departamentoBeneficiado ==("laguajira"))
+            {
+                departamentoElegido = laguajira;
+            }
+            else if (departamentoBeneficiado ==("magdalena"))
+            {
+                departamentoElegido = magdalena;
+            }
+            else if (departamentoBeneficiado ==("meta"))
+            {
+                departamentoElegido = meta;
+            }
+            else if (departamentoBeneficiado ==("nariño"))
+            {
+                departamentoElegido = nariño;
+            }
+            else if (departamentoBeneficiado ==("nortedesantander"))
+            {
+                departamentoElegido = nortedesantander;
+            }
+            else if (departamentoBeneficiado ==("putumayo"))
+            {
+                departamentoElegido = putumayo;
+            }
+            else if (departamentoBeneficiado ==("quindio"))
+            {
+                departamentoElegido = quindio;
+            }
+            else if (departamentoBeneficiado ==("risaralda"))
+            {
+                departamentoElegido = risaralda;
+            }
+            else if (departamentoBeneficiado ==("SanAndresYProvidencia"))
+            {
+                departamentoElegido = SanAndresYProvidencia;
+            }
+            else if (departamentoBeneficiado ==("santander"))
+            {
+                departamentoElegido = santander;
+            }
+            else if (departamentoBeneficiado ==("sucre"))
+            {
+                departamentoElegido = sucre;
+            }
+            else if (departamentoBeneficiado ==("tolima"))
+            {
+                departamentoElegido = tolima;
+            }
+            else if (departamentoBeneficiado ==("valledelcauca"))
+            {
+                departamentoElegido = valledelcauca;
+            }
+            else if (departamentoBeneficiado ==("vaupes"))
+            {
+                departamentoElegido = vaupes;
+            }
+            else if (departamentoBeneficiado ==("vichada"))
+            {
+                departamentoElegido = vichada;
+            }
+        }
+    }
+}
---
+++ Integrante.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    internal class Integrante
+    {
+         public static string nombre, apellido, identificacion, rol, email;
+
+        public Integrante(string nombre, string apellido, string identificacion, string rol, string email)
+
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Identificacion = identificacion;
+            this.Rol = rol;
+            this.Email = email;
+        }
+
+         public string Nombre { get => nombre; set => nombre = value; }
+         public string Apellido { get => apellido; set => apellido = value; }
+         public string Identificacion { get => identificacion; set => identificacion = value; }
+         public string Rol { get => rol; set => rol = value; }
+         public string Email { get => email; set => email = value; }
+
+        public static void addIntegrantes()
+        {
+            Console.WriteLine("Ingrese el nombre del Integrante: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el apellido del Integrante: ");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese la identificación del Integrante: ");
+            identificacion = Console.ReadLine();
+            Console.WriteLine("Ingrese el rol en el emprendimiento: ");
+            rol = Console.ReadLine();
+            Console.WriteLine("Ingrese el email del integrante: ");
+            email = Console.ReadLine();
+
+
+        }
+        
+
+
+    }
+}
